4f3b6b032efbfc970198ae44a92184474bc8576b
Ben Haefner <b.haefner@kunzleigh.com>
2019-10-23 09:27:54 -0400

    Model Coded, Enitity Design Package Installed.

---
+++ Model.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Library
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=B-Haefner-L;Database=Library;TrustedConnection=True");
+    }
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public string Isbn { get; set; }
+        public string Thumbnail { get; set; }
+        public bool  Read { get; set; }
+
+    }
+}
