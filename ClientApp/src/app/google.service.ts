bd8e5138a6c7f41f7ff24f6b85142956f37a42cf
Ben Haefner <b.haefner@kunzleigh.com>
2019-10-31 11:55:45 -0400

    Changed from behaviorsubject to eventemitter

--- ClientApp/src/app/google.service.ts
+++ ClientApp/src/app/google.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Observable, of, BehaviorSubject } from 'rxjs';
@@ -9,26 +9,26 @@ import { Book } from './models/book';
 })
 export class GoogleService {
 
-  private searchTerms: string = "";
+  private searchTerms = '';
 
-  public searchTermsBehavior: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public searchTermsBehavior = new EventEmitter();
 
   constructor(public http: HttpClient) { }
 
-  public setSearchTerms(terms: string): void{
+  public setSearchTerms(terms: string): void {
     this.searchTerms = terms;
-    this.searchTermsBehavior.next(true);
+    this.searchTermsBehavior.emit();
   }
 
   /**
    * A function to retrieve data from the Google Books API based upon
    * typed search terms.
-   * 
+   *
    * @param searchTerms A string which contains the types search terms
    * to query Google Books for.
    */
   public getSearched(): Observable<Object[]> {
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=' + this.searchTerms + '&maxResults=40';
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=' + this.searchTerms + '&maxResults=40';
     if (!this.searchTerms.trim()) {
       return of([]);
     }
@@ -40,13 +40,13 @@ export class GoogleService {
   // TODO: Consolidate with the error handling in library.service, so there is less repeating data.
     /**
    * A function to handle errors durring HTTP operations.
-   * 
+   *
    * @param operation The operation being performed when an error was thrown
    * @param result The result of the operation being performed.
    */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-      console.error(error); 
+      console.error(error);
 
       return of(result as T);
     };
--- ClientApp/src/app/search/search.component.ts
+++ ClientApp/src/app/search/search.component.ts
@@ -15,7 +15,7 @@ export class SearchComponent implements OnInit {
   public message: string;
 
   /**
-  * An array of objects, each of which contains the data of a book 
+  * An array of objects, each of which contains the data of a book
   * returned from the Google Books API.
   */
   public searched: Object[];
@@ -23,11 +23,11 @@ export class SearchComponent implements OnInit {
   /**
    * A boolean variable to indicate whether to show a loading indicator
    */
-  public showLoader: boolean = false;
+  public showLoader = false;
 
   /**
    * A function that works as the constructor for SearchComponent.
-   * 
+   *
    * @param fb A FormBuilder to easily build forms.
    * @param googleService A GoogleService to do http requests for Google Books data.
    */
@@ -40,22 +40,23 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.onParentSubmit();
   }
 
   /**
    * A function to covert data from Google Books to the format accepted
    * by this programs Web API.
-   * 
+   *
    * @param toConvert Data from Google Bookss API to be converted into
    *  a book.
    */
   private convertToBook(toConvert: any): Book {
     // Creation of book to be returned by the function, handling of null values.
-    let book: Book = {
-      title: toConvert.title ? toConvert.title : "Unknown",
+    const book: Book = {
+      title: toConvert.title ? toConvert.title : 'Unknown',
       authors: toConvert.authors ? this.extractAuthors(toConvert.authors) : null,
-      isbn: toConvert.industryIdentifiers ? toConvert.industryIdentifiers[0].identifier : "Unknown",
-      thumbnail: toConvert.imageLinks ? toConvert.imageLinks.smallThumbnail : "",
+      isbn: toConvert.industryIdentifiers ? toConvert.industryIdentifiers[0].identifier : 'Unknown',
+      thumbnail: toConvert.imageLinks ? toConvert.imageLinks.smallThumbnail : '',
       read: false,
     };
 
@@ -66,34 +67,34 @@ export class SearchComponent implements OnInit {
    * A function to handle the submitting of a search query.
    */
   public onParentSubmit() {
-    this.searched = []
-    this.message = "Searching..."
+    this.searched = [];
+    this.message = 'Searching...';
     this.showLoader = true;
     this.googleService.getSearched().subscribe(searched => {
       this.searched = searched;
       this.showLoader = false;
-      if (searched.length == 0) {
-        this.message = "There doesnt seem to be anything. Use the search bar to search for a book.";
+      if (searched.length === 0) {
+        this.message = 'There doesnt seem to be anything. Use the search bar to search for a book.';
       } else {
-        this.message = "";
+        this.message = '';
       }
     });
   }
 
   /**
    * A function to convert author data from Google Books into a format accepted
-   * by this programs Web API.  
-   * 
+   * by this programs Web API.
+   *
    * @param toExtract Author data from Google Books API to be converted into
    * authors, to be inserted into a Book object.
    */
   private extractAuthors(toExtract: any): Author[] {
     // Creation of array to be returned
-    var list: Array<Author> = [];
+    const list: Array<Author> = [];
     // Iteration over every name in Google Books author data, placing
     // all data into an author object, and pusing that object to the list.
     toExtract.forEach(extractedName => {
-      let item: Author = { name: extractedName };
+      const item: Author = { name: extractedName };
       list.push(item);
     });
     return list;
