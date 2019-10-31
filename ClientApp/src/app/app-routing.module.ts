97e2c57d115d0b4c6e3fb314cf1af05337e6cb45
Ben Haefner <b.haefner@kunzleigh.com>
2019-10-31 12:46:29 -0400

    Minor aesthetic change

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchComponent } from './search/search.component'
-import { LibraryComponent } from './library/library.component'
+import { SearchComponent } from './search/search.component';
+import { LibraryComponent } from './library/library.component';
 
 
 
